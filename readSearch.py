6ffdd1397c60e1a523f62fa915cda60c10b7e591
Nabin Maharjan <nabin247@gmail.com>
2016-04-19 18:28:31 -0500

    added functionality to compute reverse complement genomen; and refactored code

--- readSearch.py
+++ readSearch.py
@@ -1,15 +1,42 @@
 from fmIndex import FMINDEX
 import os
 #python readSearch.py <genome_folder> <read_file> buildIndex{true,false} <index_file>
-def main():
-	import sys
-	
-	genome_folder = sys.argv[1]
-	
-	SEQ = ''
-	GEN_DEL = 'NNN' # delimiter for GENOMES
-	gen_loc = {} # store the locations of individual genomes in a combined reference genome
-	
+
+def getComplementGenome(SEQ,seqType = "dna"):
+	COMP_SEQ = []
+	for char in SEQ:
+		if char == 'A':
+			if seqType == "dna":
+				COMP_SEQ.append('T')
+			else:
+				COMP_SEQ.append('U')
+		elif char == 'T' or char == 'U':
+			COMP_SEQ.append('A')
+		elif char == 'C':
+			COMP_SEQ.append('G')
+		elif char == 'G':
+			COMP_SEQ.append('C')
+	return COMP_SEQ
+
+def getAlignedLocationsFromIndividualGenomes(gen_loc,read_results):
+	orig_gen_loc = []
+	for read_results in readResults_Array:
+		for loc in read_results:
+			for gen in gen_loc.keys():
+				if loc > gen_loc[gen][0] and loc < gen_loc[gen][1]:
+					orig_gen_loc.append((gen,loc-gen_loc[gen][0])
+					#print("genome is: ", gen, "loc in concatenated genome: ", loc, "loc in original genome:", loc-gen_loc[gen][0])
+	return orig_gen_loc
+
+def readPairEndReads(read_file1,read_file2):
+	pass
+
+def searchReadPairPattern(read1,read2):
+	pass
+
+def getConcatenatedGenome(genome_folder,GEN_DEL):
+	seq = ''
+	gen_loc = {}
 	files = []
 	for f in os.listdir(genome_folder):
 		genome_file = os.path.join(genome_folder,f)
@@ -19,16 +46,41 @@ def main():
 	
 	for i in range(len(files)):
 		f,genome_file = files[i]
+		print("genome_file",f)
 		temp_SEQ = FMINDEX.readSequence(genome_file)
-		last_SEQ_len = len(SEQ)
+		last_SEQ_len = len(seq)
 		if i == len(files)-1:
-			SEQ = SEQ + temp_SEQ + "$"
+			seq = seq + temp_SEQ + "$"
 		else:
-			SEQ = SEQ + temp_SEQ + GEN_DEL
-		gen_loc[f] = (last_SEQ_len,len(SEQ))
+			seq = seq + temp_SEQ + GEN_DEL
+		gen_loc[f] = (last_SEQ_len,len(seq))
+	return seq,gen_loc
+
+def verifyReadResults(readResults,SEQ,read):
+	readResults_eval = []
+	correct_cnt = 0
+	for loc in readResults:
+		if SEQ[loc:len(read)]==read:
+			correct_cnt += 1
+			read_eval.append((loc,1))
+		else:
+			read_eval.append((loc,-1))
+	return correct_cnt/len(read_results),readResults_eval
 
 
 
+def main():
+	import sys
+	
+	genome_folder = sys.argv[1]
+	
+	GEN_DEL = 'NNN' # delimiter for GENOMES
+	
+	# get the concatenated genome and dictinary of locations for individual genomes in the concatenated genome
+	SEQ,gen_loc = getConcatenatedGenome(genome_folder,GEN_DEL)
+
+
+	#print("SEQ: ", SEQ)
 	fmIndex = FMINDEX()
 
 
@@ -54,25 +106,23 @@ def main():
 	
 	#read out_file
 	readOut_file = 'read_output.txt'
-
+	readResults_Array = []
 	with open(readOut_file,"w") as f:
-		isTesting = True
 		for read_tuple in reads:
 			read_id,read = read_tuple[0],read_tuple[1]
+			print("read",read)
 			read_results = fmIndex.searchPattern(read)
+			read_accuracy,readResults_eval = verifyReadResults(read_results,SEQ,read)
+			readResults_Array.append(read_results)
 			out_line = read_id
-			for loc in read_results:
-				out_line = out_line + "," + loc
+			locations = ''
+			for loc,error_flag in readResults_eval:
+				if locations == "":
+					locations = "{0}:{1}".format(loc,error_flag)
+				else:
+					locations = locations + ",{0}:{1}".format(loc,error_flag)
+			out_line = out_line + "," + locations + "\n"
 			f.write(out_line)
-			if isTesting:
-				break
-					
-	read_results = fmIndex.searchPattern(read)
-	for loc in read_results:
-		for gen in gen_loc.keys():
-			if loc > gen_loc[gen][0] and loc < gen_loc[gen][1]:
-				print("genome is: ", gen, "loc in concatenated genome: ", loc, "loc in original genome:", loc-gen_loc[gen][0])
-
 
 
 
